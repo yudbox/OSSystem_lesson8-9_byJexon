b5d7b9893d9c144a8cadf7561ab7470b3b6bd5bc
okean <okeanrst@gmail.com>
2019-10-08 23:02:52 +0300

    add form tasks

--- src/components/lessons/8/forms/1/index.js
+++ src/components/lessons/8/forms/1/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+function sendEmail(name, mail, message) {
+  const params = {name, mail, message};
+  let alertText = 'Email sending request in process.\nEmail params:\n';
+  alertText += Object.keys(params).map(i => `${i}: ${params[i]}`).join('\n');
+  alert(alertText);
+}
+
+class UncontrolledForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.nameRef = React.createRef();
+    this.mailRef = React.createRef();
+    // change code below this line
+
+    // change code above this line
+  }
+
+  handleSubmit = () => {
+    // change code below this line
+
+    //sendEmail(name, mail, message);
+    // change code above this line
+  };
+
+  render() {
+    return (
+      <form>
+        {/* change code below this line */}
+
+        {/* change code above this line */}
+      </form>
+    );
+  }
+}
 
 const Task = () => {
   return (
-    <div className="task" >
-      task1
+    <div>
+      <UncontrolledForm/>
     </div>
   );
 };
--- src/components/lessons/8/forms/2/index.js
+++ src/components/lessons/8/forms/2/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
+class ControlledForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // change code below this line
+
+    // change code above this line
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
+
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
+
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
 const Task = () => {
   return (
-    <div className="task" >
-      task2
+    <div>
+      <ControlledForm/>
     </div>
   );
 };
